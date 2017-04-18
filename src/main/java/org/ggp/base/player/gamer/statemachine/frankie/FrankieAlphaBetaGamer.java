57f2b9786dde401023f768f9bad223ee906d4cc8
Tyler Romero <tyler.alexander.romero@gmail.com>
2017-04-17 21:23:23 -0700

    Alpha beta updates

--- src/main/java/org/ggp/base/player/gamer/statemachine/frankie/FrankieAlphaBetaGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/frankie/FrankieAlphaBetaGamer.java
@@ -2,6 +2,7 @@ package org.ggp.base.player.gamer.statemachine.frankie;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.util.statemachine.MachineState;
@@ -14,51 +15,68 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class FrankieAlphaBetaGamer extends FrankieGamer {
 
+	private Map<Role, Integer> roleMap;
 	private Role agent;
-	private Role opp;
+	private List<Role> roles;
+	private int numRoles;
 
-	private int minScore(Role role, Move agent_move, MachineState state, StateMachine stateMachine)
-			throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException{
-		List<Move> moves = stateMachine.getLegalMoves(state, role);
-		int score = 100;
-
-		for(Move opp_move : moves) {
-			List<Move> moveList = new ArrayList<Move>();
-			if(role.equals(stateMachine.getRoles().get(0))){
-				moveList.add(opp_move);
-				moveList.add(agent_move);
-			}
-			else{
-				moveList.add(agent_move);
-				moveList.add(opp_move);
-			}
-
-			MachineState nextState = stateMachine.getNextState(state, moveList);
-
-			int result = maxScore(agent, nextState, stateMachine);
-			if (result < score) {
-				score = result;
-			}
-		}
-		return score;
+	private Role nextRole(Role currentRole){
+		int curRoleIndex = roleMap.get(currentRole);
+		int nextRoleIndex = (curRoleIndex + 1)%numRoles;
+		return roles.get(nextRoleIndex);
 	}
 
-	private int maxScore(Role role, MachineState state, StateMachine stateMachine)
+	private int minScore(Role role, List<Move> selected_moves, MachineState state, StateMachine stateMachine, int alpha, int beta)
+			throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException{
+		List<Move> moves = stateMachine.getLegalMoves(state, role);
+
+		for(Move move : moves) {
+			selected_moves.set(roleMap.get(role), move);
+			int result = 0;
+
+			if(nextRole(role).equals(agent)){
+				MachineState nextState = stateMachine.getNextState(state, selected_moves);
+				result = maxScore(agent, nextState, stateMachine, alpha, beta);
+			}
+			else{
+				result = minScore(nextRole(role), selected_moves, state, stateMachine, alpha, beta);
+			}
+
+			beta = java.lang.Math.min(beta, result);
+			if (beta <= alpha){
+				return alpha;
+			}
+
+		}
+		return beta;
+	}
+
+	private int maxScore(Role role, MachineState state, StateMachine stateMachine, int alpha, int beta)
 			throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException{
 		if (stateMachine.isTerminal(state)) {
 			return stateMachine.getGoal(state, role);
 		}
 		List<Move> moves = stateMachine.getLegalMoves(state, role);
 
-		int score = 0;
 		for(Move move : moves) {
-
-			int result = minScore(opp, move, state, stateMachine);
-			if (result > score) {
-				score = result;
+			List<Move> selected_moves = new ArrayList<Move>(numRoles);
+			for (int i =0; i<numRoles; i++){
+				selected_moves.add(move);
+			}
+			selected_moves.set(roleMap.get(agent), move);
+			int result = 0;
+			if(nextRole(role).equals(agent)){
+				MachineState nextState = stateMachine.getNextState(state, selected_moves);
+				result = maxScore(agent, nextState, stateMachine, alpha, beta);
+			}
+			else
+				result = minScore(nextRole(role), selected_moves, state, stateMachine, alpha, beta);
+			alpha = java.lang.Math.max(alpha, result);
+			if (alpha >= beta) {
+				return beta;
 			}
 		}
-		return score;
+		return alpha;
 	}
 
 	@Override
@@ -69,26 +87,36 @@ public class FrankieAlphaBetaGamer extends FrankieGamer {
 		long start = System.currentTimeMillis();	// Start timer
 		long finishBy = timeout - 1000;
 
-		for(Role r : stateMachine.getRoles()){
-			if (getRole().equals(r))
-				agent = r;
-			else
-				opp = r;
-		}
-
-		assert(stateMachine.getRoles().size() == 1);
+		roleMap = stateMachine.getRoleIndices();
+		agent = getRole();
+		roles = stateMachine.getRoles();
+		numRoles = roles.size();
+		System.out.println(""+numRoles);
 
 		List<Move> moves = stateMachine.getLegalMoves(getCurrentState(), getRole());
 		Move action = moves.get(0);
 
 		// Go through all of the legal moves
 
+		List<Move> selected_moves = new ArrayList<Move>(numRoles);
+		for (int i=0; i<numRoles; i++){
+			selected_moves.add(action);
+		}
+
 		int score = 0;
 		for(Move move : moves) {
 			System.out.println(""+move);
-
-			int result = minScore(opp, move, getCurrentState(), stateMachine);
-			System.out.println("Maxscore: "+result);
+			int alpha = 0;
+			int beta = 100;
+			selected_moves.set(roleMap.get(agent), move);
+			int result = 0;
+			if(nextRole(agent).equals(agent)){
+				MachineState nextState = stateMachine.getNextState(getCurrentState(), selected_moves);
+				result = maxScore(agent, nextState, stateMachine, alpha, beta);
+			}
+			else
+				result = minScore(nextRole(agent), selected_moves, getCurrentState(), stateMachine, alpha, beta);
+			System.out.println("Maxscore: " + result);
 			if (result == 100) {
 				action = move;
 				break;
@@ -101,7 +129,7 @@ public class FrankieAlphaBetaGamer extends FrankieGamer {
 
 		long stop = System.currentTimeMillis();		// Stop timer
 
-		// Dont touch
+		// Don't touch
 		notifyObservers(new GamerSelectedMoveEvent(moves, action, stop - start));
 		return action;
 	}
